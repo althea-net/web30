8ffc979928583d37194bfcc411d3239b4813ebb8
Justin Kilpatrick <justin@althea.net>
2021-07-04 14:46:57 -0400

    Update simulation functions for London gas
    Gas prices in London are computed differnetly. This has many
    implications, but this pr handles only a very small scope to ensure our
    gas simulations and eth_call() functions work correctly.

--- src/client.rs
+++ src/client.rs
@@ -378,9 +378,7 @@ impl Web3 {
         let gas_limit = if let Some(gl) = gas_limit {
             gl
         } else {
-            let gas = self
-                .simulated_gas_price_and_limit(our_balance.clone())
-                .await?;
+            let gas = simulated_gas_price_and_limit(our_balance.clone());
             self.eth_estimate_gas(TransactionRequest {
                 from: Some(own_address),
                 to: to_address,
@@ -454,7 +452,7 @@ impl Web3 {
 
         let payload = encode_call(sig, tokens)?;
 
-        let gas = self.simulated_gas_price_and_limit(our_balance).await?;
+        let gas = simulated_gas_price_and_limit(our_balance);
         let transaction = TransactionRequest {
             from: Some(own_address),
             to: contract_address,
@@ -605,6 +603,41 @@ impl Web3 {
 struct SimulatedGas {
     limit: Uint256,
     price: Uint256,
+}
+
+struct SimulatedGas {
+    limit: Uint256,
+    price: Uint256,
+}
+
+// Geth and parity behave differently for the Estimate gas call or eth_call()
+// Parity / OpenEthereum will allow you to specify no gas price
+// and no gas amount the estimate gas call will then return the
+// amount of gas the transaction would take. This is reasonable behavior
+// from an endpoint that's supposed to let you estimate gas usage
+//
+// The gas price is of course irrelevant unless someone goes out of their
+// way to design a contract that fails a low gas prices. Geth and Parity
+// can't simulate an actual transaction market accurately.
+//
+// Geth on the other hand insists that you provide a gas price of at least
+// 7 post London hardfork in order to respond. This seems to be because Geth
+// simply tosses your transaction into the actual execution code, so no gas
+// instantly fails.
+//
+// If this value is too low Geth will fail, if this value is higher than
+// your balance Geth will once again fail. So Geth at this juncture won't
+// tell you what the transaction would cost, just that you can't afford it.
+//
+// Max possible gas price is Uint 32 max, Geth will print warnings above 25mil
+// gas.
+//
+// This function will navigate all these restrictions in order to give you the
+// maximum valid gas possible for any simulated call
+fn simulated_gas_price_and_limit(balance: Uint256) -> SimulatedGas {
+    let price: Uint256 = 10u8.into();
+    let limit = min(25_000_000u128.into(), balance / price.clone());
+    SimulatedGas { limit, price }
 }
 
 #[test]
