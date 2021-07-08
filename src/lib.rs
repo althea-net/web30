b3db3efb8cdd892a959f0514e2a5503f046a9eac
Justin Kilpatrick <justin@althea.net>
2021-07-08 10:39:34 -0400

    Actix is a dev dependency only

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,18 @@
 #![warn(clippy::all)]
 #![allow(clippy::pedantic)]
 
-extern crate serde;
-extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
-extern crate awc;
-extern crate clarity;
-extern crate futures;
-extern crate num256;
-extern crate tokio;
 #[macro_use]
 extern crate log;
 #[macro_use]
 extern crate lazy_static;
 
+pub mod amm;
 pub mod client;
 mod erc20_utils;
 mod event_utils;
 pub mod jsonrpc;
 pub mod types;
-pub mod amm;
 
 pub use event_utils::address_to_event;
