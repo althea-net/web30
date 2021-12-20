6f379b6af30457a4259174323ce9ac809c2928dc
leviathanbeak88 <elvisdedic@outlook.com>
2021-12-20 13:06:21 +0300

    simplify error

--- src/jsonrpc/error.rs
+++ src/jsonrpc/error.rs
@@ -1,4 +1,3 @@
-use awc::error::SendRequestError as ActixError;
 use clarity::Error as ClarityError;
 use clarity::Uint256;
 use std::error::Error;
@@ -12,12 +11,7 @@ use tokio::time::error::Elapsed;
 #[derive(Debug)]
 pub enum Web3Error {
     BadResponse(String),
-    FailedToSend(ActixError),
-    JsonRpcError {
-        code: i64,
-        message: String,
-        data: String,
-    },
+    JsonRpcError(reqwest::Error),
     InsufficientGas {
         balance: Uint256,
         base_gas: Uint256,
@@ -52,12 +46,24 @@ impl From<Elapsed> for Web3Error {
     }
 }
 
+impl From<reqwest::Error> for Web3Error {
+    fn from(error: reqwest::Error) -> Self {
+        Web3Error::JsonRpcError(error)
+    }
+}
+
+impl From<serde_json::Error> for Web3Error {
+    fn from(error: serde_json::Error) -> Self {
+        Web3Error::BadResponse(error.to_string())
+    }
+}
+
 impl Display for Web3Error {
     fn fmt(&self, f: &mut Formatter) -> Result {
         match self {
             Web3Error::BadResponse(val) => write!(f, "Web3 bad response {}", val),
             Web3Error::BadInput(val) => write!(f, "Web3 bad input {}", val),
-            Web3Error::FailedToSend(val) => write!(f, "Web3 Failed to send {}", val),
+            Web3Error::JsonRpcError(val) => write!(f, "Web3 JsonRpc Failed {}", val),
             Web3Error::EventNotFound(val) => write!(f, "Web3 Failed to find event {}", val),
             Web3Error::ClarityError(val) => write!(f, "ClarityError {}", val),
             Web3Error::TransactionTimeout => write!(f, "Transaction did not enter chain in time"),
@@ -82,15 +88,6 @@ impl Display for Web3Error {
             Web3Error::CouldNotRemoveFilter(val) => {
                 write!(f, "Web3 Failed to remove filter from server {}", val)
             }
-            Web3Error::JsonRpcError {
-                code,
-                message,
-                data,
-            } => write!(
-                f,
-                "Web3 Response error code {} message {} data {:?}",
-                code, message, data
-            ),
         }
     }
 }
